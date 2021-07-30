cc0c8ce64de20b756ed5afe53e8e3e5cbf04ef68
zearkiatos <caprilespe@outlook.com>
2021-07-30 17:32:53 -0400

    Extract data and configuration gitlab-ci

--- index.php
+++ index.php
@@ -18,4 +18,31 @@ function getTeacher()
 
 $teacher = 'Pedro';
 
-echo "${getTeacher()} teachs PHP";
+echo "${getTeacher()} teachs PHP <br>";
+
+$data = 'I\'m study PHP<br>';
+
+echo "$data[0] <br>";
+
+$post = "Bacon ipsum dolor amet pastrami short ribs chislic pancetta ball tip. Andouille filet mignon bresaola pork loin shankle alcatra brisket sausage. Pig prosciutto biltong buffalo drumstick. Sausage chislic cupim fatback filet mignon turducken chuck capicola pork chop kielbasa turkey. Ham hock jerky porchetta, beef ribs beef tongue pancetta pig. Frankfurter short loin ribeye, buffalo kielbasa jowl shoulder ham hock spare ribs alcatra chislic pancetta flank porchetta pork loin. Ham hock hamburger drumstick jowl strip steak ribeye flank doner cupim turducken chicken burgdoggen swine cow.";
+
+$extract = substr($post, 0, 20);
+
+echo "$extract... [ver más]";
+
+$languages = 'javascript, php, laravel';
+
+$tags = explode(', ', $languages);
+
+echo "<pre>";
+
+var_dump($tags);
+
+$courses = ['javascript', 'php', 'laravel'];
+
+echo implode(',', $courses);
+
+$course = "      Course of PHP         ";
+$course = trim($course);
+echo "<pre>";
+echo "I want to learn $course and other courses";

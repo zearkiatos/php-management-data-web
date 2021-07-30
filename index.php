2cf2f06a314e9e5871aec69d01d384f7c12dd518
zearkiatos <caprilespe@outlook.com>
2021-07-30 18:12:50 -0400

    Data format and kubernetes configuration deployment and service

--- index.php
+++ index.php
@@ -45,4 +45,47 @@ echo implode(',', $courses);
 $course = "      Course of PHP         ";
 $course = trim($course);
 echo "<pre>";
-echo "I want to learn $course and other courses";
+echo "I want to learn $course and other courses <br>";
+
+$text = "PHP is A LANGUAGE";
+
+echo "$text <br>";
+
+echo strtolower($text) . "<br>";
+
+echo strtoupper($text) . "<br>";
+
+echo ucfirst($text) . "<br>";
+
+echo lcfirst($text) . "<br>";
+
+// Replace
+
+$slug = str_replace(' ', '-', $text);
+
+echo strtolower($slug) . "<br>";
+
+// Modification
+
+$code = 39;
+
+echo "$code <br>";
+
+echo str_pad($code, 8, '#')."<br>";
+
+echo str_pad($code, 8, '#', STR_PAD_BOTH)."<br>";
+
+echo str_pad($code, 8, '#', STR_PAD_LEFT)."<br>";
+
+
+$textWithHTML = "<h1>$text</h1>";
+
+echo "$textWithHTML<br>";
+
+echo strip_tags($text)."<br>";
+
+$spanishText = "PHP es UN LENGUAJE, año 2020, programación";
+
+echo strtoupper($spanishText)."<br>"; // monobyte
+
+echo mb_strtoupper($spanishText)."<br>"; // multibyte

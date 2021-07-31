2bf45b12f7643d05ea357d45372dbbd6b5671835
zearkiatos <caprilespe@outlook.com>
2021-07-31 17:11:22 -0400

    Review our project

--- src/Validate.php
+++ src/Validate.php
@@ -9,4 +9,14 @@ class Validate
     {
         return (bool)filter_var($value, FILTER_VALIDATE_EMAIL);
     }
+
+    public static function url($value)
+    {
+        return (bool)filter_var($value, FILTER_VALIDATE_URL);
+    }
+
+    public static function password($value)
+    {
+        return (bool)preg_match('/^[0-9]{6,9}$/', $value);
+    }
 }
--- tests/ValidateTest.php
+++ tests/ValidateTest.php
@@ -11,9 +11,33 @@ class ValidateTest extends TestCase
         $this->assertTrue($email);
     }
 
-    public function test_should_be_false_when_email_is_valid()
+    public function test_should_be_false_when_email_is_not_valid()
     {
         $email = Validate::email('i@@obsipay.com');
         $this->assertFalse($email);
+    }
+
+    public function test_should_be_true_when_url_is_valid()
+    {
+        $url = Validate::url('https://pedro.capriles.io');
+        $this->assertTrue($url);
+    }
+
+    public function test_should_be_false_when_url_is_not_valid()
+    {
+        $url = Validate::url('pedro.capriles.io');
+        $this->assertFalse($url);
+    }
+
+    public function test_should_be_true_when_password_is_not_valid()
+    {
+        $password = Validate::password('123456');
+        $this->assertTrue($password);
+    }
+
+    public function test_should_be_true_when_password_is_valid()
+    {
+        $password = Validate::password('123456deded');
+        $this->assertFalse($password);
     }
 }

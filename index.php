d2b9d00030dd12ff99393349b7f6554ec7fd6693
zearkiatos <caprilespe@outlook.com>
2021-07-31 17:26:49 -0400

    Arguments

--- index.php
+++ index.php
@@ -3,3 +3,37 @@
 require __DIR__ . '/vendor/autoload.php';
 
 var_dump(App\Validate::email('caprilespe@outlook.com'));
+
+// values
+function greet($name)
+{
+    return "Hello, $name";
+}
+
+echo "<br>";
+
+echo greet("Pedro");
+
+// references
+$course = 'PHP';
+function path(&$course)
+{
+    $course = 'Laravel';
+
+    echo $course;
+}
+echo "<br>";
+path($course);
+echo "<br>";
+echo $course;
+
+// predefine
+
+function greetings($name = 'Luis')
+{
+    return "Hello, $name";
+}
+echo "<br>";
+echo greetings();
+echo "<br>";
+echo greetings('Maria');
